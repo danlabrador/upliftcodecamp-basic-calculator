a440376e34ffad02e9c229fdd36c6b30083b3f69
cit.extension <cit.extension@gmail.com>
2024-03-04 20:20:23 +0800

    sub

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-
+"use strict";
+const subtract = (num1, num2) => {
+  if (typeof num1 !== "number" || typeof num2 !== "number") {
+    return "Both arguments must be numbers";
+  }
+  return num1 - num2;
+};
