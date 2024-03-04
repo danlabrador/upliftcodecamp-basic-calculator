1c75b3d2247abbbbc54dd559679e16b3d6dc3e26
cit.extension <cit.extension@gmail.com>
2024-03-04 20:27:44 +0800

    feat:create divide()

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 "use strict";
 const subtract = (num1, num2) => {
   if (typeof num1 !== "number" || typeof num2 !== "number") {
-    return "Both arguments must be numbers";
+    return "must be numbers";
   }
   return num1 - num2;
 };
